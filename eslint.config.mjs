a7909aed2330e06770997da7ffce32789a5f93e5
Coiness <1512133221@qq.com>
2025-11-18 19:52:09 +0800

    build:init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,9 +12,9 @@ const eslintConfig = defineConfig([
     '.next/**',
     'out/**',
     'build/**',
-    'next-env.d.ts'
+    'next-env.d.ts',
   ]),
-  eslintConfigPrettier
+  eslintConfigPrettier,
 ])
 
 export default eslintConfig
